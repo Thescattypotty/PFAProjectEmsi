aa8758d48aa1e2092ab9b08b9cc1ce2d191b0eed
Thescattypotty <bnsyahya123@gmail.com>
2024-05-04 15:23:29 +0100

    Adding test controllers due to mappers error

--- backend/src/main/java/com/example/backend/Controller/TestController.java
+++ backend/src/main/java/com/example/backend/Controller/TestController.java
@@ -1,16 +1,32 @@
 package com.example.backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.example.backend.Entity.*;
 
 import com.example.backend.Annotation.RequiresRole;
+import com.example.backend.EntityRepository.CompanyRepository;
+import com.example.backend.EntityRepository.ContactRepository;
+import com.example.backend.EntityRepository.InteractionRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+	@Autowired 
+	private ContactRepository contactRepository;
+
+	@Autowired
+	private CompanyRepository companyRepository;
+
+	@Autowired
+	private InteractionRepository interactionRepository;
 
 	@GetMapping("/all")
 	public String allAccess() {
@@ -33,5 +49,28 @@ public class TestController {
 	@RequiresRole({ "ROLE_ADMIN" })
 	public String adminAccess() {
 		return "Admin Board.";
+	}
+
+	@GetMapping("/contacts")
+	@RequiresRole({ "ROLE_USER" })
+	public ResponseEntity<List<Contact>> getContacts()
+	{
+		List<Contact> contacts = contactRepository.findAll();
+		return ResponseEntity.ok(contacts);
+	}
+
+	@GetMapping("/companys")
+	@RequiresRole({"ROLE_USER"})
+	public ResponseEntity<List<Company>> getCompanys() {
+		List<Company> compans = companyRepository.findAll();
+		return ResponseEntity.ok(compans);
+	}
+
+
+	@GetMapping("/interactions")
+	@RequiresRole({ "ROLE_USER" })
+	public ResponseEntity<List<Interaction>> getInteractions() {
+		List<Interaction> interactions = interactionRepository.findAll();
+		return ResponseEntity.ok(interactions);
 	}
 }
