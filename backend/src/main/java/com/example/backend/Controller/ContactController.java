ca30b9fd1939a3aa373814d6c0617cf6a5c355a2
Thescattypotty <bnsyahya123@gmail.com>
2024-05-20 13:05:51 +0100

    Adding Twilio and email to controller

--- backend/src/main/java/com/example/backend/Controller/ContactController.java
+++ backend/src/main/java/com/example/backend/Controller/ContactController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,10 +23,13 @@ import com.example.backend.Entity.Contact;
 import com.example.backend.Entity.Interaction;
 import com.example.backend.MapToResponse.ContactMapperResponse;
 import com.example.backend.MapToResponse.InteractionMapperResponse;
+import com.example.backend.Payload.Request.EmailRequest;
 import com.example.backend.Payload.Response.ContactResponse;
 import com.example.backend.Payload.Response.InteractionResponse;
 import com.example.backend.Services.ContactService;
 import com.example.backend.Services.InteractionService;
+import com.example.backend.Services.MailService;
+import com.example.backend.Services.TwilioService;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -40,6 +44,15 @@ public class ContactController {
     @Autowired
     private InteractionService interactionService;
 
+    @Autowired 
+    private MailService mailService;
+
+    @Autowired
+    private TwilioService twilioService;
+
+    @Value("${twilio.twiml.endpoint.url}")
+    private String twilioUrl;
+
     @PostMapping
     @RequiresPermission({"CREATE_CONTACT"})
     public ResponseEntity<ContactResponse> createContact(@RequestBody Contact contactDto)
@@ -47,6 +60,38 @@ public class ContactController {
         Contact savedContactDto = contactService.createContact(contactDto);
         return new ResponseEntity<>(ContactMapperResponse.MapToContactResponse(savedContactDto), HttpStatus.CREATED);
     }
+    
+    @GetMapping("/{id}")
+    @RequiresPermission({ "ACCESS_CONTACT" })
+    public ResponseEntity<ContactResponse> getContactById(@PathVariable("id") Long contactId) {
+        Contact contactDto = contactService.getContactById(contactId);
+        return ResponseEntity.ok(ContactMapperResponse.MapToContactResponse(contactDto));
+    }
+
+    @GetMapping
+    @RequiresPermission({ "ACCESS_CONTACT" })
+    public ResponseEntity<List<ContactResponse>> getAllcontact() {
+        List<Contact> contacts = contactService.getAllContacts();
+        return ResponseEntity.ok(
+                contacts.stream().map(ContactMapperResponse::MapToContactResponse)
+                        .collect(Collectors.toList()));
+    }
+
+    @PutMapping("/{id}")
+    @RequiresPermission({ "MODIFY_CONTACT" })
+    public ResponseEntity<ContactResponse> updatecontact(@PathVariable("id") Long contactId,
+            @RequestBody Contact contactDto) {
+        Contact contact = contactService.updateContact(contactId, contactDto);
+        return ResponseEntity.ok(ContactMapperResponse.MapToContactResponse(contact));
+    }
+
+    @DeleteMapping("/{id}")
+    @RequiresPermission({ "DELETE_CONTACT" })
+    public ResponseEntity<String> deletecontact(@PathVariable("id") Long contactId) {
+        contactService.deleteContact(contactId);
+        return ResponseEntity.ok("contact Deleted Successfully !.");
+    }
+
     @PostMapping("/{id}/interaction")
     @RequiresPermission({"CREATE_INTERSECTION","MODIFY_CONTACT"})
     public ResponseEntity<ContactResponse> addInteractionToContact(@PathVariable("id") Long contactId, @RequestBody Interaction interactionDto)
@@ -64,37 +109,35 @@ public class ContactController {
         return ResponseEntity.ok(InteractionMapperResponse.mapToInteractionResponse(UpdatedinteractionDto));
     }
 
-
-    @GetMapping("/{id}")
-    @RequiresPermission({ "ACCESS_CONTACT" })
-    public ResponseEntity<ContactResponse> getContactById(@PathVariable("id") Long contactId)
+    @PostMapping("/{id}/interaction/sendEmail")
+    @RequiresPermission({"CREATE_INTERSECTION", "MODIFY_CONTACT","MODIFY_INTESECTION"})
+    public ResponseEntity<String> SendEmail(@PathVariable("id") Long interactionId, @RequestBody EmailRequest emailRequest)
     {
-        Contact contactDto = contactService.getContactById(contactId);
-        return ResponseEntity.ok(ContactMapperResponse.MapToContactResponse(contactDto));
+        if(emailRequest.getAttachement() == null)
+        {
+            mailService.sendEmailWithAttechement(emailRequest);
+        }else{
+            mailService.sendEmail(emailRequest);
+        }
+        return ResponseEntity.ok("Email Send Successfully");
     }
     
-    @GetMapping
-    @RequiresPermission({ "ACCESS_CONTACT" })
-    public ResponseEntity<List<ContactResponse>> getAllcontact() {
-        List<Contact> contacts = contactService.getAllContacts();
-        return ResponseEntity.ok(
-            contacts.stream().map(ContactMapperResponse::MapToContactResponse)
-            .collect(Collectors.toList())
-        );
-    }
-    
-    @PutMapping("/{id}")
-    @RequiresPermission({ "MODIFY_CONTACT" })
-    public ResponseEntity<ContactResponse> updatecontact(@PathVariable("id") Long contactId,
-            @RequestBody Contact contactDto) {
-        Contact contact = contactService.updateContact(contactId, contactDto);
-        return ResponseEntity.ok(ContactMapperResponse.MapToContactResponse(contact));
+    @PostMapping("/{id}/interaction/sendSms")
+    @RequiresPermission({ "CREATE_INTERSECTION", "MODIFY_CONTACT", "MODIFY_INTESECTION" })
+    public ResponseEntity<String> SendSms(@PathVariable("id") Long interactionId,
+            @RequestBody String phoneNumber , @RequestBody String message) {
+        twilioService.sendSms(phoneNumber, message);
+        return ResponseEntity.ok("Sms Send Successfully");
     }
 
-    @DeleteMapping("/{id}")
-    @RequiresPermission({ "DELETE_CONTACT" })
-    public ResponseEntity<String> deletecontact(@PathVariable("id") Long contactId) {
-        contactService.deleteContact(contactId);
-        return ResponseEntity.ok("contact Deleted Successfully !.");
+    @PostMapping("/{id}/interaction/makeCall")
+    @RequiresPermission({ "CREATE_INTERSECTION", "MODIFY_CONTACT", "MODIFY_INTESECTION" })
+    public ResponseEntity<String> MakeCall(@PathVariable("id") Long interactionId,
+            @RequestBody String to) {
+        twilioService.makeCall(to, twilioUrl);
+        return ResponseEntity.ok("Call Pending");
     }
+
+
+   
 }
